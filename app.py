3567d44f61771f2c255573dfb207dd9ad7e164aa
aiskillupacademy <160478752+aiskillupacademy@users.noreply.github.com>
2024-10-10 08:45:56 +0000

    Initial Commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import OpenAIEmbeddings
-from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 from langchain_google_vertexai import VertexAI
 from langchain.output_parsers import PydanticOutputParser
 import pymupdf4llm
